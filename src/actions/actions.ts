d2c93297c38cc3880a7403d1aba10fb6ec303d52
Taleex <matosjax@gmail.com>
2026-03-09 21:30:16 +0000

    Image form default fix

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -8,7 +8,7 @@ export async function addPet(formData) {
         data: {
             name: formData.get("name"),
             ownerName: formData.get("ownerName"),
-            imageUrl: formData.get("imageUrl"),
+            imageUrl: formData.get("imageUrl") || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
             age: parseInt(formData.get("age")),
             notes: formData.get("notes")
         }
