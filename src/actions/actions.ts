1d2c13e17daf179d085e81cc87ebb41148a3c05a
Taleex <matosjax@gmail.com>
2026-03-09 21:25:34 +0000

    Action formdata

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,8 +2,15 @@
 
 import prisma from "@/lib/db";
 
-export async function addPet(pet) {
+export async function addPet(formData) {
+    console.log("Adding pet:", formData);
     await prisma.pet.create({
-        data: pet
+        data: {
+            name: formData.get("name"),
+            ownerName: formData.get("ownerName"),
+            imageUrl: formData.get("imageUrl"),
+            age: parseInt(formData.get("age")),
+            notes: formData.get("notes")
+        }
     });
 }
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import React, { act } from 'react'
 import { Label } from './ui/label'
 import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
-import { PetContext } from '@/contexts/pet-context-provider';
 import { usePetContext } from '@/lib/hooks';
+import { addPet } from '@/actions/actions';
 
 type PetFormProps = {
     actionType: "add" | "edit";
@@ -15,28 +14,10 @@ type PetFormProps = {
 
 export default function PetForm({actionType, onFormSubmission}: PetFormProps) {
 
-    const {handleAddPet, selectedPet, handleEditPet} = usePetContext();
-
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        const formData = new FormData(e.currentTarget);
-        const pet ={
-            name: formData.get("name") as string,
-            ownerName: formData.get("ownerName") as string,
-            imageUrl: formData.get("imageUrl") as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
-            age: +(formData.get("age") as string),
-            notes: formData.get("notes") as string,
-        };
-
-        actionType === "add" && handleAddPet(pet);
-        actionType === "edit" && handleEditPet(selectedPet!.id, pet);
-
-        onFormSubmission();
-    }
+    const { selectedPet} = usePetContext();
 
   return (
-    <form onSubmit={handleSubmit} className='flex flex-col'>
+    <form action={addPet} className='flex flex-col'>
 
         <div className='space-y-3'> 
             <div className='space-y-1'>
