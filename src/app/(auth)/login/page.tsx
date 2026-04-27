3d2e81095ee98ec2b4eeb54d79a01b5c4f63d044
Taleex <matosjax@gmail.com>
2026-04-27 20:05:58 +0100

    332 - Auth Layout

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,19 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 
 export default function page() {
   return (
-    <div>page</div>
+    <main>
+      <H1 className="mb-5 text-center">Log In</H1>
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        No account yet ?{" "}
+        <Link href="/signup" className="font-medium">
+          Sign up
+        </Link>
+      </p>
+    </main>
   )
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,6 +1,19 @@
+import AuthForm from "@/components/auth-form";
+import H1 from "@/components/h1";
+import Link from "next/link";
 
 export default function page() {
   return (
-    <div>page</div>
+    <main>
+      <H1 className="mb-5 text-center">Sign Up</H1>
+      <AuthForm />
+
+      <p className="mt-6 text-sm text-zinc-500">
+        Already have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Log In
+        </Link>
+      </p>
+    </main>
   )
 }
