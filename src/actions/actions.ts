cbdbd107d00698c982b2dc64169a4343ad53d0eb
Taleex <matosjax@gmail.com>
2026-05-21 08:15:32 +0100

    359 - add pets by correct user

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,11 +1,12 @@
 "use server";
 
-import { signIn, signOut } from "@/lib/auth";
+import { auth, signIn, signOut } from "@/lib/auth";
 import prisma from "@/lib/db";
 import { sleep } from "@/lib/utils";
 import { petFormSchema, petIdSchema } from "@/lib/validations";
 import bcrypt from "bcryptjs";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 // -- user actions--
 
@@ -37,6 +38,11 @@ export async function logOut() {
 // -- pet actions--
 export async function addPet(pet: unknown) {
     await sleep(2000);
+    
+    const session = await auth();
+    if (!session?.user) {
+        redirect("/login");
+    }
 
     const validatedPet = petFormSchema.safeParse(pet);
 
@@ -46,7 +52,9 @@ export async function addPet(pet: unknown) {
 
     try {
     await prisma.pet.create({
-        data: validatedPet.data,
+        data: {
+            ...validatedPet.data, user: { connect: { id: session.user.id } }
+        },
     });
 
 } catch (error) {
