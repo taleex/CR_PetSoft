0425064cca478197d3cb8c2387cc920b07da31ba
Taleex <matosjax@gmail.com>
2026-05-26 08:04:54 +0100

    379 - Payment page setup layout

--- src/app/(payment)/layout.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function layout() {
-  return (
-    <>
-        <h1 className="text-2xl font-bold mb-4">Payment layout</h1>
-    </>
-  )
-}
--- src/app/(payment)/payment/page.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function page() {
-  return (
-    <div>
-      Payment page
-    </div>
-  )
-}
